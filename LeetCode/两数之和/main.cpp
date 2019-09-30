a1ba43f31bab8bd48ad12dc847bcb1abd7938661
zkk258369 <zkk258369@163.com>
2019-09-30 22:51:36 +0800

    摘录了官网目前最快的方法

--- LeetCode/两数之和/main.cpp
+++ LeetCode/两数之和/main.cpp
@@ -1,4 +1,5 @@
-﻿/*
+﻿//1.两数之和
+/*
 给定一个整数数组 nums 和一个目标值 target
 请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
 
@@ -16,11 +17,12 @@
 #include<unordered_map>
 #include<vector>
 #include<iterator>
+#include<algorithm>
 
 using std::vector;
 
 
-//暴力法
+//暴力法 200ms
 //class Solution
 //{
 //public:
@@ -45,26 +47,52 @@ using std::vector;
 //	}
 //};
 
-//哈希表
-class Solution
-{
+//哈希表 12ms
+//class Solution
+//{
+//public:
+//	vector<int> twoSum(vector<int>& nums, int target)
+//	{
+//		vector<int> ansArr(2, -1);
+//		std::unordered_map<int, int> record;//创建哈希表
+//		for (int i = 0; i < nums.size(); i++)
+//		{
+//			int tmp = target - nums[i];
+//			if (record.find(tmp) != record.end())
+//			{
+//				ansArr[0] = i;
+//				ansArr[1] = record[tmp];
+//				break;
+//			}
+//			record[nums[i]] = i;
+//		}
+//		return ansArr;
+//	}
+//};
+
+//执行用时为 8 ms 的范例
+class Solution {
 public:
-	vector<int> twoSum(vector<int>& nums, int target)
-	{
-		vector<int> ansArr(2, -1);
-		std::unordered_map<int, int> record;//创建哈希表
-		for (int i = 0; i < nums.size(); i++)
-		{
-			int tmp = target - nums[i];
-			if (record.find(tmp) != record.end())
-			{
-				ansArr[0] = i;
-				ansArr[1] = record[tmp];
+	vector<int> twoSum(vector<int>& nums, int target) {
+		vector<int> res;
+		vector<int> tmp(nums);
+		sort(tmp.begin(), tmp.end());//排序
+		int i = 0, j = tmp.size() - 1;
+		//排序后，从前后和后面同时开始找
+		while (i < j){
+			if (tmp[i] + tmp[j] == target){
+				res.push_back(std::find(nums.begin(), nums.end(), tmp[i]) - nums.begin());
+				res.push_back(nums.size() - (std::find(nums.rbegin(), nums.rend(), tmp[j]) - nums.rbegin()) - 1);
 				break;
 			}
-			record[nums[i]] = i;
+			else if (tmp[i] + tmp[j] > target){
+				j--;
+			}
+			else{
+				i++;
+			}
 		}
-		return ansArr;
+		return res;
 	}
 };
 
@@ -75,12 +103,13 @@ int main()
 	int target = 9;
 	int num[] = { 2, 7, 11, 15 };
 
-	vector<int> nums(num, num + 2);
-	vector<int> ans(2, -1);
+	vector<int> nums(num, num + 4);
+	vector<int> ans(1);
+
 	ans = s1.twoSum(nums, target);
 
 	std::ostream_iterator<int> out(std::cout, " ");
-	copy(ans.begin(), ans.end(),out);
+	copy(ans.begin(), ans.end(), out);
 	std::cout << std::endl;
 	system("pause");
 
