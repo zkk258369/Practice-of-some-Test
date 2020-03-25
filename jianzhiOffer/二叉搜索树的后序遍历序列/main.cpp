affff8189fd9c065ae4d5e7a0dee90d7c6205513
zkk <zkk258369@163.com>
2020-03-25 10:26:22 +0800

    master

--- jianzhiOffer/二叉搜索树的后序遍历序列/main.cpp
+++ jianzhiOffer/二叉搜索树的后序遍历序列/main.cpp
@@ -8,14 +8,26 @@ using std::vector;
 class Solution
 {
 public:
+    bool judge(vector<int>& vec, int l, int r)
+    {
+        if(l >= r)
+            return true;
+        int i = l;
+        while(vec[i]<vec[r] && i<r)
+            ++i;
+        for(int j=i+1; j<r; ++j)
+        {
+            if(vec[j] < vec[r])
+                return false;
+        }
+        return judge(vec, l, i-1) && judge(vec, i, r-1);
+    }
     bool VerifySquenceOfBST(vector<int> sequence)
     {
-        
+        bool res = false;
+        if(sequence.empty())
+            return res;
+        res = judge(sequence, 0, sequence.size()-1);
+        return res;
     }
 };
-
-int main()
-{
-
-    return 0;
-}
