cbbbfbb93bef459c66fe11c4c9f894c6946b36eb
zkk <zkk258369@163.com>
2020-04-05 23:54:36 +0800

    二叉树的下一个节点

---
+++ jianzhiOffer/二叉树的下一个节点/main.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+typedef struct TreeLinkNode
+{
+    int val;
+    TreeLinkNode *left;
+    TreeLinkNode *right;
+    TreeLinkNode *next;
+    TreeLinkNode(int x) 
+        :val(x), left(NULL), right(NULL), next(NULL) 
+    {}
+}TreeLinkNode;
+
+class Solution
+{
+public:
+    TreeLinkNode* GetNext(TreeLinkNode* pNode)
+    {
+        TreeLinkNode* p = pNode;
+        
+    }
+};
