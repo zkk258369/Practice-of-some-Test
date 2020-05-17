add1c4c1baad0b5da010c2062116d4e50a0b1fa0
zkk <zkk258369@163.com>
2020-05-17 13:58:05 +0800

    BinarySearch

--- jianzhiOffer/11.旋转数组的最小数字.cpp
+++ jianzhiOffer/11.旋转数组的最小数字.cpp
@@ -10,72 +10,24 @@ NOTE：给出的所有元素都大于0，若数组大小为0，请返回0。
 整个数组都有序
 二分查找中indexFirst,indexLast,indexMid三个指向的数字相同
 */
-#include<iostream>
-#include<vector>
-#include<iterator>
+#include<bits/stdc++.h>
+using namespace std;
 
-using std::vector;
-
-class Solution {
-public:
-	int minNumInRotateArray(vector<int> rotateArray)
-	{
-		if (rotateArray.size() == 0)
-		{
-			return 0;
-		}
-		int indexFirst = 0;
-		int indexLast = rotateArray.size() - 1;
-		int indexMid = indexFirst;//解决数组本来全部有序
-		while (rotateArray[indexFirst] >= rotateArray[indexLast])
-		{
-			if (indexLast - indexFirst == 1)
-			{
-				indexMid = indexLast;
-				break;
-			}
-			indexMid = (indexFirst + indexLast) / 2;
-
-			//如果indexFirst,indexLast,indexMid三个指向的数字相同
-			//则只能顺序查找
-			if (rotateArray[indexFirst] == rotateArray[indexMid] &&
-				rotateArray[indexMid] == rotateArray[indexLast])
-				return minNumberInRotateArray(rotateArray);
-
-			if (rotateArray[indexFirst] <= rotateArray[indexMid])
-			{
-				indexFirst = indexMid;
-			}
-			else if (rotateArray[indexLast] >= rotateArray[indexMid])
-			{
-				indexLast = indexMid;
-			}
-		}//end while
-		return rotateArray[indexMid];
-	}//end minNumInRotateArray
-private:
-	int minNumberInRotateArray(vector<int> rotateArray)
-	{
-		std::vector<int>::iterator it = rotateArray.begin();
-		while (it != rotateArray.end())
-		{
-			if (*it > *(it + 1))
-			{
-				return *(it + 1);
-			}
-			it++;
-		}
-	}//end minNumberInRotateArray按顺序查找
-};
-
-
-int main()
+class Solution
 {
-	int arr[] {3, 4, 5, 1, 2};
-	vector<int> vec(arr, arr + 5);
-	Solution s1;
-	int ans = s1.minNumInRotateArray(vec);
-	std::cout << ans << std::endl;
-
-	return 0;
-}
+public:
+    int minArray(vector<int>& arr)
+    {
+        if(arr.size() == 0) return 0;
+        int left = 0;
+        int right = arr.size()-1;
+        while(left < right)
+        {
+            int mid = left + (right-left)/2;
+            if(arr[mid] > arr[right]) left = mid+1;
+            else if(arr[mid] < arr[right]) right = mid;
+            else right -= 1;
+        }
+        return arr[left];
+    }
+};
