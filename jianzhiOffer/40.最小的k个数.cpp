96ec330093680f92a14c7297d968ce0881b0f2e7
zkk <zkk258369@163.com>
2020-07-17 11:06:10 +0800

    add

---
+++ jianzhiOffer/40.最小的k个数.cpp
@@ -0,0 +1,97 @@
+#include<iostream>
+#include<queue>
+#include<time.h>
+using namespace std;
+
+class Solution // topk
+{
+public:
+	vector<int> getLeastNumbers(vector<int>& arr, int k)
+	{
+		vector<int> res(k);
+        if(arr.empty() || k<1) return res;
+		priority_queue<int> pque;
+		int len = arr.size();
+		for (int i = 0; i < k; i++)
+		{
+			pque.push(arr[i]);
+		}
+		for (int i = k; i < len; i++)
+		{
+			if (pque.top() > arr[i])
+			{
+				pque.pop();
+				pque.push(arr[i]);
+			}
+		}
+		for (int i = 0; !pque.empty(); i++)
+		{
+			res[i] = pque.top();
+			pque.pop();
+		}
+		
+		return res;
+	}
+};
+
+
+class Solution  // quicksort
+{
+public:
+	int partition(vector<int>& arr, int l, int r)
+	{
+		int pivot = arr[r];
+		int i = l - 1;
+		for (int j = l; j <= r - 1; j++)
+		{
+			if (arr[j] <= pivot)
+			{
+				i = i + 1;
+				swap(arr[i], arr[j]);
+			}
+		}
+		swap(arr[i + 1], arr[r]);
+		return i + 1;
+	}
+	int randomized_partition(vector<int>& arr, int l, int r)
+	{
+		int i = rand() % (r - l + 1) + l;
+		swap(arr[r], arr[i]);
+		return partition(arr, l, r);
+	}
+	void randomized_select(vector<int>& arr, int l, int r, int k)
+	{
+		if (l >= r) return;
+		int pos = randomized_partition(arr, l, r);
+		int num = pos - l + 1;
+		if (k == num) return;
+		else if (k < num) randomized_select(arr, l, pos - 1, k);
+		else randomized_select(arr, pos + 1, r, k - num);
+	}
+	vector<int> getLeastNumbers(vector<int>& arr, int k)
+	{
+		srand((unsigned)time(nullptr));
+		randomized_select(arr, 0, arr.size() - 1, k);
+		vector<int> res(k);
+		for (int i = 0; i < k; i++)
+		{
+			res[i] = arr[i];
+		}
+		return res;
+	}
+};
+
+int main()
+{
+	int ar[] = { 0, 0, 0, 2, 0, 5, 0 };
+	int len = sizeof(ar) / sizeof(ar[0]);
+	vector<int> arr(ar, ar + len);
+	Solution s;
+	vector<int> res = s.getLeastNumbers(arr, 4);
+	for (auto i : res)
+	{
+		cout << i << " ";
+	}
+	cout << endl;
+	return 0;
+}
---
+++ 复习题目/10.cpp
@@ -0,0 +1,111 @@
+//least recently used
+#include<iostream>
+#include<unordered_map>
+#include<string.h>
+using namespace std;
+
+struct DuListNode
+{
+	int key;
+	int value;
+	DuListNode* pre;
+	DuListNode* next;
+	DuListNode() :pre(nullptr), next(nullptr){}
+	DuListNode(int _key, int _value)
+		:key(_key), value(_value), pre(nullptr), next(nullptr)
+	{}
+};
+
+class LRUCache
+{
+public:
+	LRUCache(int ca) :size(0), capacity(ca)
+	{
+		head = BuyNode();
+		tail = BuyNode();
+		head->next = tail;
+		tail->pre = head;
+	}
+	~LRUCache()
+	{
+		while (head->next != tail)
+		{
+			DeleteTail();
+		}
+		delete head;
+		delete tail;
+	}
+
+	int Get(int _key)
+	{
+		if (cache.count(_key) == 0) return -1;
+		DuListNode* p = cache[_key];
+		p->next->pre = p->pre;
+		p->pre->next = p->next;
+		AddHead(p);
+		return p->value;
+	}
+	void Put(int _key, int _value)
+	{
+		if (cache.count(_key) == 0)
+		{
+			if (size == capacity)
+			{
+				DeleteTail();
+			}
+			DuListNode* newNode = BuyNode();
+			newNode->key = _key;
+			newNode->value = _value;
+			AddHead(newNode);
+			cache[_key] = newNode;
+			size++;
+		}
+		else
+		{
+			DuListNode* p = cache[_key];
+			p->next->pre = p->pre;
+			p->pre->next = p->next;
+			p->value = _value;
+			AddHead(p);
+		}
+	}
+private:
+	DuListNode* BuyNode()
+	{
+		DuListNode* newNode = new DuListNode();
+		memset(newNode, 0, sizeof(DuListNode));
+		return newNode;
+	}
+	void AddHead(DuListNode* p)
+	{
+		p->next = head->next;
+		p->pre = head;
+		head->next = p;
+		p->next->pre = p;
+	}
+	void DeleteTail()
+	{
+		DuListNode* p = tail->pre;
+		cache.erase(p->key);
+		p->pre->next = tail;
+		tail->pre = p->pre;
+		delete p;
+		--size;
+	}
+private:
+	unordered_map<int, DuListNode*> cache;
+	DuListNode* head;
+	DuListNode* tail;
+	int size;
+	int capacity;
+};
+
+int main()
+{
+	LRUCache lru(1);
+	lru.Put(2, 1);
+	int x = lru.Get(1);
+	cout << x << endl;
+
+	return 0;
+}
