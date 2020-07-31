fd2f4722f631b388477e7634ac71693b18a3261a
zkk <zkk258369@163.com>
2020-07-31 22:38:35 +0800

    update

--- LeetCode/回溯法/51.N皇后.cpp
+++ LeetCode/回溯法/51.N皇后.cpp
@@ -55,3 +55,58 @@ private:
     vector<int> vec;
     int sum;
 };
+
+class Solution // NonRecursive
+{
+public:
+    vector<vector<string>> solveNQueens(int n)
+    {
+        vec.resize(n + 1);
+        sum = 0;
+        int i = 1;
+		vec[i] = 0;
+		while (i > 0)
+		{
+			if (i <= n) vec[i] += 1;
+			while (i <= n && vec[i] <= n && !Place(i)) vec[i] += 1;
+			if (i > n)
+			{
+				sum += 1;
+                chessBoard.resize(sum);
+                for (int row = 1; row <= n; row++)
+                {
+                    string str;
+                    for (int col = 1; col <= n; col++)
+                    {
+                        if (vec[row] == col) str.push_back('Q');
+                        else str.push_back('.');
+                    }
+                    chessBoard[sum-1].push_back(str);
+                }
+				i--;
+			}
+			else if (vec[i] > n) i--;
+			else
+			{
+				i++;
+				if(i <= n) vec[i] = 0;
+			}
+		}
+        return chessBoard;
+    }
+private:
+	bool Place(int row)
+	{
+		for (int i = 1; i < row; i++)
+		{
+			if (vec[row] == vec[i] || abs(vec[i] - vec[row]) == abs(i - row))
+            {
+                return false;
+            }
+		}
+		return true;
+	}
+    vector<vector<string>> chessBoard;
+    vector<int> vec;
+    int sum;
+};
