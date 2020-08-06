e741542c4b1d2d86315d3b6bece27d1cc05ac94f
zkk <zkk258369@163.com>
2020-08-06 09:45:35 +0800

    add

--- LeetCode/47.全排列2.cpp
+++ LeetCode/47.全排列2.cpp
@@ -1,13 +1,36 @@
 #include<iostream>
 #include<vector>
-#include<stack>
+#include<set>
 using namespace std;
 
 class Solution
 {
+    vector<vector<int>> res;
+    int n;
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums)
     {
-        
+        n = nums.size();
+        TrackBack(nums, 0, n-1);
+        return res;
+    }
+    void TrackBack(vector<int>& nums, int k, int m)
+    {
+        if(k == m)
+        {
+            res.push_back(nums);
+        }
+        set<int> setSwap;
+        for(int j=k; j<=m; j++)
+        {
+            if(setSwap.find(nums[j]) != setSwap.end())
+            {
+                continue;
+            }
+            setSwap.insert(nums[j]);
+            swap(nums[j], nums[k]);
+            TrackBack(nums, k+1, m);
+            swap(nums[j], nums[k]);
+        }
     }
 };
