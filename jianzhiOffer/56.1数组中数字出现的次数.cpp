599914056f2b263a69621724961175f08bb622ac
zkk <zkk258369@163.com>
2020-10-11 15:29:45 +0800

    二分法

--- jianzhiOffer/56.1数组中数字出现的次数.cpp
+++ jianzhiOffer/56.1数组中数字出现的次数.cpp
@@ -32,3 +32,36 @@ public:
 		return res;
 	}
 };
+
+
+//只出现一次的数字，其余数字出现两次，数字都有序
+// 用二分法做，根据这个数字两侧的数字下标的奇偶不同。
+int fun(vector<int>& vec)
+{
+	int left = 0;
+	int right = vec.size() - 1;
+	while (left < right)
+	{
+		int mid = left + (right - left) / 2;
+		if (vec[mid] == vec[mid - 1])
+		{
+			if (mid % 2 == 0) right = mid - 2;
+			else left = mid + 1;
+		}
+		else if (vec[mid] == vec[mid + 1])
+		{
+			if (mid % 2 == 0) left = mid + 2;
+			else right = mid - 1;
+		}
+		else return vec[mid];
+	}
+	return vec[left];
+}
+
+int main()
+{
+	vector<int> vec = { 1, 1, 2, 2, 3, 4, 4, 5 };
+	int res = fun(vec);
+	cout << res << endl;
+	return 0;
+}
